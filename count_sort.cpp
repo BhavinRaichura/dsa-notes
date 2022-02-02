90ab8a3b6435efdfea49c7893da9f4f63c6acaf0
BhavinRaichura <bhavinraichura28@gmail.com>
2022-02-03 01:05:11 +0530

    commit

---
+++ count_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* count sort algo and its problems
+
+vector<int> countSort(int arr[]){
+    int length = sizeof(arr)/sizeof (arr[0]);
+    maxnum = max(arr.begin,arr.end);
+}
+*/
+
+int main(){
+
+    int arr[] = {2,4,3,7,1}
+    maxnum = max(arr.begin,arr.end);
+    cout<<maxnum;
+    return 0;
+
+}
