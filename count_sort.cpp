850336284a95779e86dcad822efb8e97b59d582f
BhavinRaichura <bhavinraichura28@gmail.com>
2022-02-03 02:32:53 +0530

    count sort function

--- count_sort.cpp
+++ count_sort.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 /* count sort algo and its problems */
 
+/* Count sort:
+ * - It works by counting the number of objects having distinct key values (kind of hashing). Then doing some arithmetic to calculate the position of each object in the output sequence.
+ * - It is works for array which having +ve elements
+ * - Counting sort can be extended to work for negative inputs also, but you need to modify code.
+ * - Counting sort uses a partial hashing to count the occurrence of the data object in O(1).
+ *
+ * - Input sequence is between range 1 to 10K
+ * - Input range: [1, 10K]
+ * - Time Complexity : O(n); n is the number of elements.
+ * - Space Complexity : O(n+k); space complexity depend on max element in array to create zero array(zero array size = K).
+ * */
+
+
+// --------------count sort function---------------------
+
 void countSort(int arr[],int n){
     int maxnum=0;
     for(int i=0;i<n;i++){
@@ -23,11 +38,19 @@ void countSort(int arr[],int n){
     }
 }
 
+
+
+
+
+//--------------- print array--------------------
+
 void printArr(auto arr,int len){
     for(int i=0;i<len;i++)
         cout<<"  "<<arr[i];
 }
 
+
+//--------------------main function-----------------
 
 int main(){
 
@@ -38,3 +61,4 @@ int main(){
     return 0;
 
 }
+
